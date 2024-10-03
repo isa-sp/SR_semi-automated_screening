edf0091cea04d721e01cd9862a21df879186386a
Isa Spiero <90386963+isa-sp@users.noreply.github.com>
2024-10-03 17:46:03 +0200

    cleaned functions

--- functions/generate_recall_table_prop.py
+++ functions/generate_recall_table_prop.py
@@ -4,26 +4,32 @@ import pandas as pd
 def generate_recall_table_prop(evaluation, props, n_simulations, data_type = 'original'):
     
     proportions = []
+    
     for i in range(0, len(props)):
         proportions.append(str(int(props[i]*100)) + '%')
     length_prop = len(proportions) * n_simulations
     df_prop = pd.DataFrame()
+    
     for key, value in evaluation.items():
         names = key.split('_')
         review = [names[0]] * length_prop
+        
         if data_type == 'original':
             train_model = [names[1]] * length_prop
             feature_model = [names[2]] * length_prop
             query_model = [names[3]] * length_prop
+            
         if data_type == 'adapted':
             total_records = [names[1]] * length_prop
             rel_records = [names[2]] * length_prop
             train_model = [names[3]] * length_prop
             feature_model = [names[4]] * length_prop
             query_model = [names[5]] * length_prop
+            seed = [names[6]] * length_prop
         simulations = []
 
         props = proportions * n_simulations
+        
         for i in range(1, n_simulations+1):
             simulations.append(list(np.repeat(i, len(proportions))))
         recalls = value[0][0]['Recall']
@@ -33,8 +39,9 @@ def generate_recall_table_prop(evaluation, props, n_simulations, data_type = 'or
 
         if data_type == 'original':
             df_sim = pd.DataFrame(list(zip(review, sim_number, train_model, feature_model, query_model, simulations, props, recalls)),columns = ['Review', 'Simulation number', 'Train model', 'Feature model', 'Query model', 'Simulation', 'percentage of records screened','recall'])
+
         if data_type == 'adapted':
-            df_sim = pd.DataFrame(list(zip(review, total_records, rel_records, sim_number, train_model, feature_model, query_model, simulations, props, recalls)),columns = ['Review', 'Total records', 'Relevant records', 'Simulation number', 'Train model', 'Feature model', 'Query model', 'Simulation', 'percentage of records screened','recall'])
+            df_sim = pd.DataFrame(list(zip(review, seed, total_records, rel_records, sim_number, train_model, feature_model, query_model, simulations, props, recalls)),columns = ['Review', 'Seed', 'Total records', 'Relevant records', 'Simulation number', 'Train model', 'Feature model', 'Query model', 'Simulation', 'percentage of records screened','recall'])
 
         df_prop = pd.concat([df_prop, df_sim])
 
@@ -43,6 +50,7 @@ def generate_recall_table_prop(evaluation, props, n_simulations, data_type = 'or
     df_prop['Review_full'] = ""
 
     df_prop['Review_full'] = df_prop['Review'].apply(lambda x: x.replace("Prog", "Prognosis "))
+    
     for i in range(0, len(df_prop['Review_full'])):
         if "Int" in df_prop['Review_full'][i]:
             df_prop['Review_full'][i] = df_prop['Review_full'][i].replace("Int", "Intervention ")
