d396c2ac2f868b8af2cc671219d41d842b0b7a9b
Isa Spiero <90386963+isa-sp@users.noreply.github.com>
2024-10-04 13:25:44 +0200

    Add files via upload

---
+++ src/submit_1_job.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#export PATH="$PATH:/home/julius_te/ispiero/miniconda3/condabin/"
+#source /home/julius_te/ispiero/miniconda3/etc/profile.d/conda.sh
+#conda activate SR_environment  
+
+SCRIPT="simulate_adapted_datasets.py"
+BS=$SLURM_ARRAY_TASK_ID
+export TORCH_HOME=/hpc/local/Rocky8/julius_te/ispiero/
+
+python $SCRIPT -sim_id $BS
